5c3ae4921d516c01723d300b1255106304c35052
EidAbdullahi <eidabdullahi10@gmail.com>
2020-12-18 00:06:33 +0300

    commit changes

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lola:1234@localhost/ticket'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:Access@localhost/ticket'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:Access@localhost/ticket'
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test':TestConfig
+
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME='samiroahmed000@gmail.com'
+export MAIL_PASSWORD='eid12345'
+export SECRET_KEY='secret'
+
+python3.6 manage.py runserver
